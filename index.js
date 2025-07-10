4ce20ff6a605a8fa19c908e14e749f993ea2dd23
Anirudh Singh <anirudhrathore2003@gmail.com>
2025-07-10 14:53:23 +0530

    rate limit inc

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const rateLimit= require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 200, // limit each IP to 100 requests per windowMs
   message: "Too many requests, please try again later.",
 });
 
